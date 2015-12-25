7aa2ddb481670b9140020a388b2c5499e53e3169
Johannes Schickling <schickling.j@gmail.com>
2015-12-25 14:28:30 +0100

    Initial Commit

---
+++ Icomoon/Icomoon.h
@@ -0,0 +1,19 @@
+//
+//  Icomoon.h
+//  Icomoon
+//
+//  Created by Johannes Schickling on 25/12/2015.
+//  Copyright © 2015 Optonaut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Icomoon.
+FOUNDATION_EXPORT double IcomoonVersionNumber;
+
+//! Project version string for Icomoon.
+FOUNDATION_EXPORT const unsigned char IcomoonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Icomoon/PublicHeader.h>
+
+
